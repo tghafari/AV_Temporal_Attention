a202ec3a5d5df8972fdda772775ae4c9cc79be8d
Tara Ghafari <tara.ghafari@gmail.com>
2020-11-27 18:32:53 +0000

    debugging

--- Stimuli Programs/Visual Stimulus/RFTVars.m
+++ Stimuli Programs/Visual Stimulus/RFTVars.m
@@ -3,7 +3,7 @@ function [visRFTFreq,PCRefreshRate,frmPhaseStep] = RFTVars
 %Frequency tagging variables and phase steps
 
 visRFTFreq    = 63;
-PCRefreshRate = Screen('FrameRate',window);       % Output from PTB - NOT Propixx frame rate
+PCRefreshRate = Screen('NominalFrameRate',window);% Output from PTB - NOT Propixx frame rate
 frmPhaseStep  = visRFTFreq*2*pi/PCRefreshRate/12; %12 is the propixx frame rate / screen frame rate
 
 end
