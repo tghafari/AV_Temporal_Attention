526545a63045c748c54893a62901012f2e4190a1
tghafari <tara.ghafari@gmail.com>
2020-11-30 12:39:05 +0000

    vis RFT program debugged + Aud RFT added

--- Stimuli Programs/Visual Stimulus/RFTVars.m
+++ Stimuli Programs/Visual Stimulus/RFTVars.m
@@ -1,9 +1,10 @@
 function [visRFTFreq,PCRefreshRate,frmPhaseStep] = RFTVars(window)
-%[visRFTFreq,PCRefreshRate,frmPhaseStep,visRFTPhase] = RFTVars(window,visRFTPhase)
+%[visRFTFreq,PCRefreshRate,frmPhaseStep] = RFTVars(window)
 %Frequency tagging variables and phase steps
 
 visRFTFreq    = 63;
-PCRefreshRate = Screen('NominalFrameRate',window);% Output from PTB - NOT Propixx frame rate
+PCRefreshRate = 60;
+%Screen('NominalFrameRate',window);% Output from PTB - NOT Propixx frame rate
 frmPhaseStep  = visRFTFreq*2*pi/PCRefreshRate/12; %12 is the propixx frame rate / screen frame rate
 
 end
