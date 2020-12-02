aa6c125a5121612cc603f5385fb7b09913d25ea9
tghafari <tara.ghafari@gmail.com>
2020-12-02 09:51:48 +0000

    photodiode path + noise for auditory(incomplete)  added

--- Stimuli Programs/Visual Stimulus/RFTVars.m
+++ Stimuli Programs/Visual Stimulus/RFTVars.m
@@ -1,11 +1,13 @@
-function [visRFTFreq,PCRefreshRate,frmPhaseStep] = RFTVars(window)
-%[visRFTFreq,PCRefreshRate,frmPhaseStep] = RFTVars(window)
+function [visRFTFreq,PCRefreshRate,frmPhaseStep,photoDiodePatch] = RFTVars(window,windowRect,display)
+%[visRFTFreq,PCRefreshRate,frmPhaseStep,photoDiodePatch] = RFTVars(window,windowRect,display)
 %Frequency tagging variables and phase steps
+%photodiode patch with size 1 degree * 1 degree
 
 visRFTFreq    = 63;
-PCRefreshRate = 60;
+PCRefreshRate = 60; %for debugging purposes leave this 60, but needs to be changed to Screen(NominalFrame...)
 %Screen('NominalFrameRate',window);% Output from PTB - NOT Propixx frame rate
 frmPhaseStep  = visRFTFreq*2*pi/PCRefreshRate/12; %12 is the propixx frame rate / screen frame rate
 
+photoDiodePatch = [windowRect(3)-angle2pix(display,1),windowRect(4)-angle2pix(display,1),windowRect(3),windowRect(4)];
 end
 
