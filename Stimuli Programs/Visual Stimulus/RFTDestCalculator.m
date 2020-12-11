dbae629a11436bd69c62e94d202415690f2e3b14
tghafari <tara.ghafari@gmail.com>
2020-12-11 13:01:13 +0000

    txts are modified for RFT

--- Stimuli Programs/Visual Stimulus/RFTDestCalculator.m
+++ Stimuli Programs/Visual Stimulus/RFTDestCalculator.m
@@ -1,6 +1,8 @@
-function [destCoordRFT,destRectRFT] = RFTDestCalculator(destVisStim,windowRect)
-%[destCoordRFT,destRectRFT] = RFTdestCalculator(destVisStim,windowRect)
+function [destCoordRFT,destRectRFT,destCentreRFT] = RFTDestCalculator(destVisStim,windowRect)
+%[destCoordRFT,destRectRFT,destCentreRFT] = RFTDestCalculator(destVisStim,windowRect)
 % makes four destination rectangles and four coordinates of centres for RFT
+%destVisStim -> destination rectangle size in pixles
+%windowRect -> output of Screen('OpenWindow'...) - windows size
 
 scrnWidth = windowRect(3); 
 screnHght = windowRect(4);
@@ -17,7 +19,8 @@ centreRect2 = CenterRectOnPoint(destVisStim,3*scrnWidth/4,1*screnHght/4);
 centreRect3 = CenterRectOnPoint(destVisStim,1*scrnWidth/4,3*screnHght/4);
 centreRect4 = CenterRectOnPoint(destVisStim,3*scrnWidth/4,3*screnHght/4);
 
-destRectRFT = {centreRect1,centreRect2,centreRect3,centreRect4};
+destRectRFT   = {centreRect1,centreRect2,centreRect3,centreRect4};            %for rectangle destination - images
+destCentreRFT = {cntrCoordRect1,cntrCoordRect2,cntrCoordRect3,cntrCoordRect4};%for destination centres - fixation cross and texts
 
 end
 
